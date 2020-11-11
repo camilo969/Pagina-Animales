faab31be5e2af06fde45beaa1701fa3f80b2840a
camilo969 <cbeltrancastillo@gmail.com>
2020-11-10 21:28:21 -0300

    Se agrego Crispy Forms con bootstrap, ademas de otros cambios menores

--- animalcare/animalcare/settings.py
+++ animalcare/animalcare/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.blog',
     'apps.Usuario',
+    'crispy_forms',
     
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
